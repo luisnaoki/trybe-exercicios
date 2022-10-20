28eb9f097a4187ab761afe225c2747fd36233879
luisnaoki <luis_naoki@hotmail.com>
2022-10-20 14:57:59 -0400

    primeiro exercicio

---
+++ 01-Fundamentos/secao-04-introducao-a-javascript/script.js
@@ -0,0 +1,18 @@
+let age = 20;
+let name ="Hulk";
+console.log(age);
+console.log(name);  
+
+const myName ="Luis Naoki";
+const birthCity = "Japão";
+let birthYear = 1997;
+birthYear= 1800;
+birthCity= "Brasil"
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+/*
+(Não é possível efetuar a alteração do constante);
