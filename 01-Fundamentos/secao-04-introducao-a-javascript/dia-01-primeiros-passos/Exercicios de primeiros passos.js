e8d82494e7d8685d06d213848a008ce67dd9ed37
luisnaoki <luis_naoki@hotmail.com>
2022-10-21 12:11:43 -0400

    efetuei as atividade

--- 01-Fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/Exercicios de primeiros passos.js
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/Exercicios de primeiros passos.js
@@ -62,4 +62,68 @@ if(angulo1+angulo2+angulo3 === 180){
   console.log("erro 404");
 }
 
+let movimentoXadrez;
+
+switch(movimentoXadrez){
+  case "peão":
+    console.log("A movimentação é apenas 1 quadrado e come nas diagonais");
+    break;
+
+  case "cavalo":
+    console.log("Movimentação em L, e come apenas em L");
+    break;
+
+  case "torre":
+    console.log ("Movimentação frontal e lateral");
+    break;
+  
+  case "bispo":
+    console.log ("movimentação em diagonal");
+    break;
+  
+  case "rainha":
+    console.log("movimentação diagonal,frontal, vertical, quanta quiser");
+    break;
+
+  case "rei":
+    console.log("movimentação uma casa diagonal, frontal, vertical");
+  
+  default:
+    console.log("não é uma peça de xadrez");
+}
+
+let nota;
+
+if(nota<0 || nota>100){
+  console.log("cara é um Deus, error 404");
+}
+if(nota>=90){
+  console.log("Nota A meu patrão");
+}else if (nota>=80){
+  console.log("nota B ");
+}else if(nota>=70){
+  console.log("nota c");
+}else if(nota >= 60){
+  console.log("nota D")
+}else{
+  console.log("reprovado")
+}
  
+let number1;
+let number2;
+let number3;
+
+if(number1 % 2 === 0 || number2 % 2 === 0 || number3 %2 ===0 ){
+  console.log("true");
+}
+if(number1number1 % 2 !== 0 && number2 % 2 !==0 && number3 %2!==0 ){
+  console.log("false");
+}
+
+  // o custo de um produto e seu valor de venda.
+  const custo= 12;
+  const venda= 20;
+  let quantidadeVenda = 100;
+  const valorCustoTotal = custo+custo*20/100;
+  const lucro = venda*quantidadeVenda- valorCustoTotal*quantidadeVenda;
+
