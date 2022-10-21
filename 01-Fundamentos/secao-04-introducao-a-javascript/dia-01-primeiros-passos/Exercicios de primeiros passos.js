4e15698ed7a7686b2ff1fc9e9f732ee58b9bd494
luisnaoki <luis_naoki@hotmail.com>
2022-10-21 10:53:50 -0400

    exercicios realizados

---
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/Exercicios de primeiros passos.js
@@ -0,0 +1,65 @@
+const numero1 = 6;
+const numero2 = 3;
+
+//adição
+console.log(numero1+numero2);
+
+//subtração
+console.log(numero1-numero2);
+
+//multiplicação
+console.log (numero1*numero2);
+
+//divisão
+console.log (numero1/numero2);
+
+//módulo
+console.log (numero1/numero2)
+
+const a = 20;
+const b = 10;
+
+if (a>b){
+  console.log ("a é maior que b");
+
+}else{
+  console.log("b é maior que a");
+}
+
+const c = 5;
+const d= 6;
+const e= 7;
+
+if(c>d && c>e){
+  console.log ("c é o maior de todos");
+  
+} else if(d>c && d>e){
+  console.log("d é o maior de todos");
+
+}else{
+  console.log("e é o maior de todos");
+}
+
+const number = 10;
+
+if(number>0){
+  console.log("Positivão meu chapa");
+}else if(number<0){
+  console.log("negativo patrão");
+}else{
+  console.log("é zero");
+}
+
+const angulo1 = 60;
+const angulo2 = 80;
+const angulo3 = 40;
+
+if(angulo1+angulo2+angulo3 === 180){
+  console.log("é um triangulo");
+}else if(angulo1+angulo2+angulo3 !== 180){
+  console.log("não é um triangulo");
+}else{
+  console.log("erro 404");
+}
+
+ 
