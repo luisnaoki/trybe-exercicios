7cd15ba29c5f1e2c541d287eb87b5c20ad92556f
luisnaoki <luis_naoki@hotmail.com>
2022-10-25 15:37:43 -0400

    exercicios de função

---
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-04-objetos/Função/Boas Vindas.js
@@ -0,0 +1,6 @@
+//Crie uma função boas vindas
+function boasVindas(nome){
+  return 'Boas Vindas, ' + nome
+
+}
+console.log(boasVindas('Naoki'))
---
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-04-objetos/Função/Função.js
@@ -0,0 +1,6 @@
+function nomeDaFuncao (param1, param2 ){
+  let resultado = param1 + param2;
+  return resultado;  
+};
+param1= 10;
+param2= 5;
