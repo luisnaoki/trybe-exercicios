c107537112eeb4316d63ebe453755fd16e67cfdc
luisnaoki <luis_naoki@hotmail.com>
2022-10-25 16:03:56 -0400

    exercicios de aulas

--- 01-Fundamentos/secao-04-introducao-a-javascript/dia-04-objetos/Função/Boas Vindas.js
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-04-objetos/Função/Boas Vindas.js
@@ -3,4 +3,22 @@ function boasVindas(nome){
   return 'Boas Vindas, ' + nome
 
 }
-console.log(boasVindas('Naoki'))
+console.log(boasVindas('Naoki'));
+
+//crie uma função para somar dois números;
+function soma(par1,par2){
+  return par1 + par2;
+};
+console.log(soma(4,2));
+console.log(soma(20,10));
+
+//Crie uma função que calcule os números de um jogo de vôlei
+function calcularPontos(vitorias,Empates){
+  let resultado = 0;
+  resultado = (vitorias*2)+ Empates;
+  return resultado
+}
+console.log(calcularPontos(1,2))
+
+//Para vitória, vale 2 pontos
+//Para empate, vale 1 ponto
