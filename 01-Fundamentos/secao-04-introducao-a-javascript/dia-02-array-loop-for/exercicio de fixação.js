4d030a56a1e7f2ebdf73f58858baa6dc8a300511
luisnaoki <luis_naoki@hotmail.com>
2022-10-25 14:26:59 -0400

    dia 4

---
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-02-array-loop-for/exercicio de fixação.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let i = 0; i<numbers.length; i++);{
+console.log(numbers[i]);
+}
+
+
+for(let i = 0; i < numbers.length; i++) {
+  soma += numbers[i];
+}
+console.log(soma);
---
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-02-array-loop-for/for.js
@@ -0,0 +1,4 @@
+let número = 7;
+for (let contador = 1; contador <= 9; contador = contador+1);{
+  console.log(número*contador);
+}
