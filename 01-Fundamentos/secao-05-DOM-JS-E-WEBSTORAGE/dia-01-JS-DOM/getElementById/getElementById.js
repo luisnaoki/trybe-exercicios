7b1106f93e669951620f157d8d7fc07d0f3604fe
luisnaoki <luis_naoki@hotmail.com>
2022-11-01 17:21:40 -0400

    exercicios do dia 1

---
+++ 01-Fundamentos/secao-05-DOM-JS-E-WEBSTORAGE/dia-01-JS-DOM/getElementById/getElementById.js
@@ -0,0 +1,4 @@
+document.getElementById("page-title").innerText = "Hobbit";
+document.getElementById("second-paragraph").innerText = "Rapaz, que vida em? Não tem dificuldade nessa matéria, porém sou burro."
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "blue";
