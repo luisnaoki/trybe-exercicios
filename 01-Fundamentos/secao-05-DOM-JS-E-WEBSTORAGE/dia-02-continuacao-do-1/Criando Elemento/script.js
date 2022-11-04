ad48c1408ca31164a660d4c8304bd9c0b747fa0e
luisnaoki <luis_naoki@hotmail.com>
2022-11-04 10:51:59 -0400

    fazendo as atividades

---
+++ 01-Fundamentos/secao-05-DOM-JS-E-WEBSTORAGE/dia-02-continuacao-do-1/Criando Elemento/script.js
@@ -0,0 +1,20 @@
+const pai = document.getElementById('pai');
+const irmaoElementoOndeVoceEsta = document.createElement('section');
+irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmaoElementoOndeVoceEsta);
+
+const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+const filhoElementoOndeVoceEsta = document.createElement ('section');
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+const primeiroFilhoDoFilho = document.getElementById ("primeiroFilhoDoFilho");
+const filhoDoPrimeiroFilhoDoFilho = document.createElement ('section');
+filhoDoPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+const terceiroFilho = filhoDoPrimeiroFilhoDoFilho
+.parentElement
+.parentElement
+.nextElementSibling;
+
